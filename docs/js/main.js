9be2739a7de4f8bfe2f406a129dc6287eadb255c
Vladimir.Zaharov <zaxar219@mail.ru>
2018-12-15 00:52:01 +0300

    dz-hom

---
+++ docs/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	
+	// Скрипт для показа и скрытия выпадающего меню на смартфонах
+	//Создали переменные
+	var pull = $("#navigation-toggle");
+	var menu = $(".navigation ul");
+	
+	
+	//Описание событие при нажатии на ссылку
+	$(pull).on("click", function(e){
+		//Отменяем стандартное повидение ссылки в браузере
+		e.preventDefault();
+		//Открываем/Скрываем меню
+
+		menu.slideToggle();
+		
+		//Добовляем модификатор --activ
+		$(this).toggleClass('navigation__toggle-button--active');
+	});
+
+	
+	$(window).resize(function(){
+	    var w = $(window).width();
+	    if(w > 768) {
+	        menu.removeAttr('style');
+	        pull.removeClass('navigation__toggle-button--active');
+	    } else {
+
+	    }
+	});
+
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 768 && nav.is(':hidden')) {
+			nav.removeAttr('style');
+		}
+	});
+	
+	// Вызов слайдера owl-carousel
+	$("#top-slider").owlCarousel({
+		singleItem: true,
+	});
+	
+	
+
+});
